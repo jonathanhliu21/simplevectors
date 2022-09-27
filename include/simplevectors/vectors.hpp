52ef64e7e5078ec930badbea7c5e365bc0571897
Jonathan Liu <jonathanhliu21@gmail.com>
2022-09-26 17:42:37 -0700

    add implementations of friend functions

--- include/simplevectors/vectors.hpp
+++ include/simplevectors/vectors.hpp
@@ -6,4 +6,28 @@
 #include "core/vector2d.hpp"
 #include "core/vector3d.hpp"
 
+#ifdef SVECTOR_EXPERIMENTAL_FEATURES_
+namespace svector {
+template <std::size_t D1, std::size_t D2, typename T1, typename T2>
+bool operator<(Vector<D1, T1> lhs, Vector<D2, T2> rhs) {
+  return lhs.compare(rhs) < 0;
+}
+
+template <std::size_t D1, std::size_t D2, typename T1, typename T2>
+bool operator>(Vector<D1, T1> lhs, Vector<D2, T2> rhs) {
+  return lhs.compare(rhs) > 0;
+}
+
+template <std::size_t D1, std::size_t D2, typename T1, typename T2>
+bool operator<=(Vector<D1, T1> lhs, Vector<D2, T2> rhs) {
+  return lhs.compare(rhs) <= 0;
+}
+
+template <std::size_t D1, std::size_t D2, typename T1, typename T2>
+bool operator>=(Vector<D1, T1> lhs, Vector<D2, T2> rhs) {
+  return lhs.compare(rhs) >= 0;
+}
+} // namespace svector
+#endif
+
 #endif
