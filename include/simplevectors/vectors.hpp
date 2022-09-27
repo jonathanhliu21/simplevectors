d25c8001861f8e33ce6be1b9241bdb69c3b99a1f
Jonathan Liu <jonathanhliu21@gmail.com>
2022-09-26 18:12:10 -0700

    add to combiner script

--- include/simplevectors/vectors.hpp
+++ include/simplevectors/vectors.hpp
@@ -6,6 +6,7 @@
 #include "core/vector2d.hpp"
 #include "core/vector3d.hpp"
 
+// COMBINER_PY_START
 #ifdef SVECTOR_EXPERIMENTAL_FEATURES_
 namespace svector {
 template <std::size_t D1, std::size_t D2, typename T1, typename T2>
@@ -29,5 +30,6 @@ bool operator>=(const Vector<D1, T1> &lhs, const Vector<D2, T2> &rhs) {
 }
 } // namespace svector
 #endif
+// COMBINER_PY_END
 
 #endif
