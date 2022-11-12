730fb72bd088886c504e3017e558a83d7418169f
Jonathan Liu <jonathanhliu21@gmail.com>
2022-11-12 08:38:59 -0800

    add embed files

---
+++ include/simplevectors/embed.h
@@ -0,0 +1,10 @@
+/**
+ * embed.h
+ *
+ * Copyright (c) 2022 Jonathan Liu. All rights reserved.
+ * MIT License
+ */
+
+#ifndef INCLUDE_SVECTOR_EMBED_H_
+#define INCLUDE_SVECTOR_EMBED_H_
+#endif
---
+++ include/simplevectors/embed_stl.hpp
@@ -0,0 +1,10 @@
+/**
+ * embed_stl.hpp
+ *
+ * Copyright (c) 2022 Jonathan Liu. All rights reserved.
+ * MIT License
+ */
+
+#ifndef INCLUDE_SVECTOR_EMBED_STL_HPP_
+#define INCLUDE_SVECTOR_EMBED_STL_HPP_
+#endif
