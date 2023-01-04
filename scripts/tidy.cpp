4054b52a4f4128f90246a7ac81e5374e1a20cdd9
Jonathan Liu <jonathanhliu21@gmail.com>
2023-01-04 14:43:01 -0800

    add clang-tidy

---
+++ scripts/tidy.cpp
@@ -0,0 +1,12 @@
+/**
+ * This file is solely for clang-tidy to analyze the simplevectors library.
+ *
+ * It assumes normal use of the library (using no experimental features or
+ * operators that are class members).
+ */
+
+#include "simplevectors/embed.h"
+#include "simplevectors/embed.hpp"
+#include "simplevectors/vectors.hpp"
+
+int main() { return 0; }
